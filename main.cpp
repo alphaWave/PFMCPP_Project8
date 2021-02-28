b0f1589f78cdbf492ffa86e8250052daea78d1a8
alphaWave <alphaWave@gmx.net>
2021-02-28 16:28:12 +0000

    Completed task 1

--- main.cpp
+++ main.cpp
@@ -44,6 +44,12 @@ your task:
 #include <vector>
 #include <algorithm>
 #include <cassert>
+#include "Car.h"
+#include "Highway.h"
+#include "HighwayPatrol.h"
+#include "Motorcycle.h"
+#include "SemiTruck.h"
+#include "Vehicle.h"
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
